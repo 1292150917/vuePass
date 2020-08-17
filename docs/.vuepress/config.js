b647f4eca9e67c8e5616baa21b7d1c6411d32113
张自方 <1292150917@qq.com>
2020-08-17 13:53:30 +0800

    初始化模板

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,66 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: miss zhang
+ * @Date: 2020-08-17 13:13:11
+ * @LastEditors: zhang zi fang
+ * @LastEditTime: 2020-08-17 13:51:14
+ */
+module.exports = {
+    title: 'Hello Dave',
+    description: 'Hello, my friend!',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: `/favicon.ico`
+        }]
+    ],
+    themeConfig: {
+        sidebar: {
+            "/foo/": [
+                {
+                    title: "pc模块",
+                    collapsable: false,
+                    path: "/foo/",
+                    children: [
+                        {
+                            title: "我是第二级",
+                            collapsable: false,
+                            path: "/foo/two",
+                        }
+                    ]
+                }, {
+                    title: "moblie模块",
+                    collapsable: false,
+                    children: [
+                        '/foo/',
+                        '/foo/one',
+                        '/foo/two',
+                    ]
+                }
+            ]
+        },
+        nav: [
+            {
+                text: "指南",
+                link: "/foo/",
+            },
+            {
+                text: "手册",
+                link: "/handbook/",
+            },
+            {
+                text: "gitee",
+                link: "/handbook1/",
+            },
+            // {
+            //     text: "工具",
+            //     items: [{ text: "github", link: "/" }],
+            // },
+        ],
+        // sidebarDepth: 3,
+    },
+    dest: './docs/.vuepress/dist',
+    ga: '',
+    evergreen: true,
+}
---
+++ docs/foo/one.md
@@ -0,0 +1,10 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: miss zhang
+ * @Date: 2020-08-17 13:19:40
+ * @LastEditors: zhang zi fang
+ * @LastEditTime: 2020-08-17 13:41:30
+-->
+[Home](/)
+ceshifew
